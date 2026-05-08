d6cbf47844365b6be138c820be8d52b10cebddae
Thando <thandotxb@gmail.com>
2026-05-09 00:22:32 +0200

    Add files via upload
    divided pages an added final touches for presentation

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("This is a webpage design. All elements on the site are for demonstration purposes and serve no purpose")
+
+function openMenu(){
+           document.getElementById("pages").style.left="0";
+        }
+
+function closeMenu(){
+           document.getElementById("pages").style.left="100vw";
+            
+        }
